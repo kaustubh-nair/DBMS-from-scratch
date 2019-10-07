bb17bf6095fa677108c09e1829f122c80f64d95d
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-10-07 18:40:32 +0530

    Remove unneccessary files

--- debug.sh
+++
@@ -1,3 +0,0 @@
-rm a.out
-gcc -g bst.c pds.c patient.c patient_runner.c
-gdb a.out 
--- init.sh
+++
@@ -1,5 +0,0 @@
-rm *.ndx
-rm *.dat
-gcc pds.c bst.c contact.c contact_loader.c
-./a.out scandemo contact_dump.txt
-./a.out newdemo contact_dump.txt
